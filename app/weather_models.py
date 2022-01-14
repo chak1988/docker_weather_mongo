7c38cd9a3b230c992c4289298ebabb70382a24d6
chak1988 <88chak88@gmail.com>
2022-01-14 17:14:28 +0200

    Initial commit

--- app/weather_models.py
+++ app/weather_models.py
@@ -3,7 +3,7 @@ import os
 
 
 c = me.DEFAULT_HOST = 'mongo'
-me.connect('weather_db')
+me.connect('weather_db', host = 'mongo')
 
 class Gallary(me.Document):
     title = me.StringField(required = True, min_length = 2)
