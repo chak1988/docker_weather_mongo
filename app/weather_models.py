97942236352fdf60c3d14bd4bf2de8c831cb4d9e
chak1988 <88chak88@gmail.com>
2022-01-14 19:09:33 +0200

    webhook commit

--- app/weather_models.py
+++ app/weather_models.py
@@ -2,9 +2,8 @@ import mongoengine as me
 import os
 
 
-MONGODB_URL = "mongodb://mongodb/dev"
-config = globals()[os.environ['MONGO_DB_ADDR']]
-me.connect('weather_db', host = config.MONGODB_URL)
+
+me.connect('weather_db', host = 'mongodb')
 
 class Gallary(me.Document):
     title = me.StringField(required = True, min_length = 2)
@@ -14,7 +13,6 @@ class Gallary(me.Document):
         return cls.objects.get(title = title)
 
 if __name__ == "__main__":
-    print(config)
     list_of_images = ['partial_clouds.jpg', 'Sunny_weather.jpg', 'Thunder_clouds.jpeg', 'Thunder_storm.jpg']
     for img in list_of_images:
         with open(f'images/{img}', 'rb') as image:
