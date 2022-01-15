962cca05bcf027d674c7a535382da6f29e5d719e
chak1988 <88chak88@gmail.com>
2022-01-15 18:26:13 +0200

    webhook commit

--- app/weather_models.py
+++ app/weather_models.py
@@ -3,7 +3,7 @@ import os
 
 
 
-me.connect('weather_db')#, host ='mongo', port =27017)
+me.connect('my_db', host="mongodb://mongo:27017/my_db")
 
 class Gallary(me.Document):
     title = me.StringField(required = True, min_length = 2)
