e15c518ccf81fd75981e3a38c0822622612f3f47
chak1988 <88chak88@gmail.com>
2022-01-18 17:45:52 +0200

    webhook commit

--- mongo.sh
+++ mongo.sh
@@ -13,7 +13,7 @@ docker-entrypoint.sh mongod --logpath /var/log/mongodb.log --logappend &
 # Wait until mongo logs that it's ready (or timeout after 60s)
 COUNTER=0
 grep -q 'waiting for connections on port' /var/log/mongodb.log
-while [[ $? -ne 0 && $COUNTER -lt 20 ]] ; do
+while [[ $? -ne 0 && $COUNTER -lt 10 ]] ; do
     sleep 2
     let COUNTER+=2
     echo "Waiting for mongo to initialize... ($COUNTER seconds so far)"
